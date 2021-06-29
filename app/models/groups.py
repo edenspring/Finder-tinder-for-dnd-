b7cf7adac9c61e008f95d0af91e431777ee5fc8a
Bill Adams <vikingbill@gmail.com>
2021-06-29 16:04:56 -0400

    add group model

---
+++ app/models/groups.py
@@ -0,0 +1,10 @@
+from .db import db
+
+class Group(db.Model):
+  __tablename__ = "groups"
+
+  id= db.Column(db.Integer, primary_key=True, nullable=False)
+  user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+  name = db.Column(db.String(75), nullable=False)
+  game_rules = db.Column(db.String(50))
+  recruiting = db.Column(db.Boolean, default=True)
