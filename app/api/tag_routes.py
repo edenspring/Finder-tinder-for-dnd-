f47315dfeb73bf5017efbee915f5c427606e2702
Bill Adams <vikingbill@gmail.com>
2021-07-01 15:39:55 -0400

    begin work on groups, about to merge with main and create new branch

--- app/api/tag_routes.py
+++ app/api/tag_routes.py
@@ -1,8 +1,6 @@
 from flask import Blueprint, jsonify, session, request
 from app.models import Tag, db
-from app.forms import LoginForm
-from app.forms import SignUpForm
-from flask_login import current_user, login_user, logout_user, login_required
+from flask_login import current_user, login_required
 
 tag_routes = Blueprint('tag', __name__)
 
@@ -10,7 +8,6 @@ tag_routes = Blueprint('tag', __name__)
 @login_required
 def add_tag():
   res = request.get_json()
-  print('++++', res)
   new_tag = Tag(**res)
   db.session.add(new_tag)
   db.session.commit()
@@ -23,18 +20,13 @@ def add_tag():
 def delete_tag(id):
   user_id = int(current_user.id)
   deletion_tag = Tag.query.get(id)
-  print(user_id)
-  print(deletion_tag.parent_id())
   if (user_id != deletion_tag.parent_id()):
-    print('NOT GON DELETE')
     return "Not allowed"
   elif (deletion_tag.parent_id() == user_id):
-    print ('GON DELETE')
     db.session.delete(deletion_tag)
     db.session.commit()
     return "deleted"
   else:
-    print('AM CONFUSE')
     return "unknown issue"
 
 @tag_routes.route('/<int:id>', methods=['GET'])
---
+++ react-app/src/components/groups/group.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+const Group = () => {
+  const group = useSelector((state)=> state.group)
+}
