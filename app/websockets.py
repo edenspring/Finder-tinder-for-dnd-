990139e231ec29a2d9d71d6cd7a8be848f1d6a1e
Bill Adams <vikingbill@gmail.com>
2021-07-09 14:12:32 -0400

    chat correctly sending to back end, creating DB entry and sending usable data back to front end

--- app/websockets.py
+++ app/websockets.py
@@ -1,7 +1,8 @@
 import os
 
+from datetime import date, datetime
 from flask_socketio import SocketIO, emit, join_room, leave_room, send
-
+from .models import db, Message
 
 origins = []
 
@@ -19,12 +20,27 @@ socketio = SocketIO(cors_allowed_origins=origins)
 
 @socketio.on("chat")
 def handle_chat(data):
-  room = data['chatId']
-  emit("chat", data, room=room)
+    print('```data```', data)
+    message = Message(**{
+        'user_id': data['user_id'],
+        'chat_id': data['chat_id'],
+        'content': data['content'],
+    })
+    db.session.add(message)
+    db.session.commit()
+    print(message.to_dict())
+    chat_data = {
+        'user': data['username'],
+        'content': data['content'],
+        'created_at': str(datetime.now()),
+    }
+    room = str(data['chat_id'])
+    emit("chat", chat_data, room=room)
+
 
 @socketio.on('join')
 def on_join(data):
-  username = data['username']
-  room = data['chatId']
-  join_room(room)
-  
+    username = data['username']
+    room = str(data['chatId'])
+    print('room', room)
+    join_room(room)
--- react-app/src/components/chats/chat.js
+++ react-app/src/components/chats/chat.js
@@ -1,52 +1,80 @@
-import {useDispatch, useSelector} from 'react-redux';
-import {useParams} from 'react-router';
-import React, {useEffect, useState, useRef} from 'react';
-import {io} from 'socket.io-client';
-
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router";
+import React, { useEffect, useState, useRef } from "react";
+import { io } from "socket.io-client";
 
 let socket;
 
-function Chat({props}){
-  console.log(props)
-  const user = useSelector((state)=>state.session.user)
-  const {chat} = props;
+function Chat({ props }) {
+  console.log(props);
+  const user = useSelector((state) => state.session.user);
+  const { chat } = props;
   const dispatch = useDispatch();
-  const [messages, setMessages] = useState([])
+  const [messages, setMessages] = useState([]);
+  const [chatInput, setChatInput] = useState("");
 
-  useEffect(()=>{
+  useEffect(() => {
     (async () => {
       const res = await fetch(`/api/chats/${chat.id}/messages`);
       const data = await res.json();
-      for (const key in data){
-        setMessages((messages)=> [...messages, data[key]])
+      for (const key in data) {
+        setMessages((messages) => [...messages, data[key]]);
       }
-    })()
+    })();
 
     socket = io();
-    socket.emit('join', {chatId: chat.id, username:user.username})
+    socket.emit("join", { chatId: chat.id, username: user.username });
 
-    socket.on('chat', (chat)=>{
+    socket.on("chat", (chat) => {
+      console.log('SKEEBAP', chat);
       setMessages((messages) => [...messages, chat])
-    })
+    });
+
     return () => {
-        socket.disconnect();
-        setMessages([])
-      }
-  },[chat])
+      socket.disconnect();
+      setMessages([]);
+    };
+  }, [chat]);
+
+  const sendChat = (e) => {
+    e.preventDefault();
+    socket.emit("chat", {
+      user_id: user.id,
+      username: user.username,
+      content: chatInput,
+      chat_id: chat.id,
+    });
+    setChatInput("");
+  };
   return (
     <>
-    <div className = "chat_body__div" id='chat_window'>
-      {messages.length? (<div className='messages_container__div'>
-        {messages.map((message, index) => (
-          <div className='message_content__div' key={`message.no${message.id}`}>
-            {JSON.stringify(message)}
+      <div className="chat_body__div" id="chat_window">
+        {messages.length ? (
+          <div className="messages_container__div">
+            {messages.map((message, index) => (
+              <div
+                className="message_content__div"
+                key={`message.no${index}`}
+              >
+                {console.log(message)}
+                <div className="message_username__div">{message.user} says</div>
+                <div className="message_text__div"> {message.content} </div>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>):'Connect to your new match! Say something....'}
-    </div>
+        ) : (
+          "Connect to your new match! Say something...."
+        )}
+      </div>
+      <form onSubmit={sendChat} className="chat_box">
+        <input
+          className="chat_input"
+          value={chatInput}
+          onChange={(e) => setChatInput(e.target.value)}
+        />
+      </form>
     </>
-  )
-
+  );
 }
 
-export default Chat
+export default Chat;
